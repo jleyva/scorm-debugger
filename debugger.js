252170b6bbfb2f47619e95a1e8df40b9079965cc
Juan Leyva <juanleyvadelgado@gmail.com>
2015-12-14 18:08:34 +0100

    Added automatic-tests

--- debugger.js
+++ debugger.js
@@ -133,6 +133,173 @@
         return;
     }
 
+    function randomNumber(min, max) {
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+
+    function randomString() {
+        return (Math.random() + 1).toString(36).substring(10);
+    }
+
+    function checkExpectedValues(output, values) {
+        var value, error;
+
+        for (var el in values) {
+            value = API.LMSGetValue(el);
+            error = API.LMSGetLastError();
+            if (error != '0') {
+                output.append('<p style="color: red">Error getting: ' + el +' with value ' + values[el] + '. ' + API.LMSGetErrorString(error) + '</p>');
+            }
+            if (value !== API.LMSGetValue(el)) {
+                output.append('<p style="color: red">Error getting: ' + el +' expected value was' + values[el] + ', returned is: ' + value + '</p>');
+            }
+        }
+    }
+
+    function launchTests() {
+        var output = $('#tests-output');
+        // Get last error.
+        if (API.LMSGetLastError() == '0') {
+            output.append('<p>Last error OK</p>');
+        }
+        // Set some values.
+        var statuses = ['passed', 'completed', 'failed', 'incomplete', 'browsed', 'not attempted'];
+        var values = {
+            'cmi.core.lesson_location': randomString(),
+            'cmi.core.lesson_status': statuses[randomNumber(0, 5)],
+            'cmi.core.score.raw': randomNumber(1, 10),
+            'cmi.core.score.min': 0,
+            'cmi.core.score.max': 10,
+            'cmi.suspend_data': randomString(),
+            'cmi.comments': randomString(),
+            'cmi.student_preference.audio': randomNumber(1, 10),
+            'cmi.student_preference.language': 'en',
+            'cmi.student_preference.speed': randomNumber(1, 10),
+            'cmi.student_preference.text': randomNumber(0, 1)
+        };
+        var value, error;
+
+        for (var el in values) {
+            API.LMSSetValue(el, values[el]);
+            error = API.LMSGetLastError();
+
+            if (error != '0') {
+                output.append('<p style="color: red">Error setting: ' + el +' with value ' + values[el] + '. ' + API.LMSGetErrorString(error) + '</p>');
+            }
+        }
+        output.append('<p>All values set</p>');
+
+        // Get the values (and check everything is correct).
+        checkExpectedValues(output, values);
+        output.append('<p>All values retrieved</p>');
+
+        // Commit the changes.
+        API.LMSCommit("");
+        error = API.LMSGetLastError();
+        if (error != '0') {
+            output.append('<p style="color: red">Error calling LMSCommit. ' + API.LMSGetErrorString(error) + '</p>');
+        }
+        output.append('<p>Values commited</p>');
+
+        // Get again after commiting the changes.
+        checkExpectedValues(output, values);
+        output.append('<p>All values retrieved</p>');
+
+        // Create some objectives.
+        var objectivesCount = randomNumber(4, 10);
+        var objectivesIds = {};
+
+        values = {};
+        for (var i=0; i<objectivesCount; i++) {
+            values['cmi.objectives.' + i + '.id'] = randomString();
+            objectivesIds[i] = values['cmi.objectives.' + i + '.id'];
+            values['cmi.objectives.' + i + '.score.raw'] = randomNumber(1, 10);
+            values['cmi.objectives.' + i + '.score.min'] = 0;
+            values['cmi.objectives.' + i + '.score.max'] = 10;
+            values['cmi.objectives.' + i + '.status'] = statuses[randomNumber(0, 5)];
+        }
+
+        for (el in values) {
+            API.LMSSetValue(el, values[el]);
+            error = API.LMSGetLastError();
+
+            if (error != '0') {
+                output.append('<p style="color: red">Error setting: ' + el +' with value ' + values[el] + '. ' + API.LMSGetErrorString(error) + '</p>');
+            }
+        }
+
+        // Test the number of objectives created is correct.
+        var lmsCount = API.LMSGetValue('cmi.objectives._count');
+        if (lmsCount !== objectivesCount) {
+            output.append('<p style="color: red">Expecting ' + objectivesCount+ ' objectives found ' + lmsCount + '</p>');
+        } else {
+            output.append('<p>Objectives count correct: ' + objectivesCount + '</p>');
+        }
+
+        // Check retrieved values.
+        checkExpectedValues(output, values);
+        output.append('<p>All objectives retrieved</p>');
+
+        // Create some interactions.
+        var interactionsCount = randomNumber(4, 10);
+        var interactionsObjectivesCount = {};
+
+        values = {};
+        var types = ['true-false', 'choice', 'fill-in', 'matching', 'performance', 'sequencing', 'likert', 'numeric'];
+        var results = ['correct', 'wrong', 'unanticipated', 'neutral'];
+        for (var i=0; i<interactionsCount; i++) {
+            values['cmi.interactions.' + i + '.id'] = randomString();
+            values['cmi.interactions.' + i + '.time'] = randomNumber(10, 20) + ':' + randomNumber(10, 59) + ':' + randomNumber(10, 59);
+            values['cmi.interactions.' + i + '.type'] = types[randomNumber(0, 7)];
+            values['cmi.interactions.' + i + '.weighting'] = randomNumber(0, 1);
+            values['cmi.interactions.' + i + '.student_response'] = randomNumber(0, 5);
+            values['cmi.interactions.' + i + '.result'] = results[randomNumber(0, 3)];
+            values['cmi.interactions.' + i + '.latency'] = randomNumber(10, 20) + ':' + randomNumber(10, 59) + ':' + randomNumber(10, 59);
+
+            var tmpCount = randomNumber(0, objectivesCount);
+            interactionsObjectivesCount[i] = tmpCount;
+            for (var j=0; j < tmpCount; j++) {
+                values['cmi.interactions.' + i + '.objectives.' + j + '.id'] = objectivesIds[j];
+                values['cmi.interactions.' + i + '.correct_responses.' + j + '.pattern'] = randomString();
+            }
+
+        }
+
+        for (el in values) {
+            API.LMSSetValue(el, values[el]);
+            error = API.LMSGetLastError();
+
+            if (error != '0') {
+                output.append('<p style="color: red">Error setting: ' + el +' with value ' + values[el] + '. ' + API.LMSGetErrorString(error) + '</p>');
+            }
+        }
+
+        // Test the number of interactions created is correct.
+        var lmsCount = API.LMSGetValue('cmi.interactions._count');
+        if (lmsCount !== interactionsCount) {
+            output.append('<p style="color: red">Expecting ' + interactionsCount+ ' interactions found ' + lmsCount + '</p>');
+        } else {
+            output.append('<p>Interactions count correct: ' + interactionsCount + '</p>');
+        }
+
+        for (i=0; i < interactionsCount; i++) {
+            var lmsCount = API.LMSGetValue('cmi.interactions.'+i+'.objectives._count');
+            if (lmsCount !== interactionsObjectivesCount[i]) {
+                output.append('<p style="color: red">Expecting ' + interactionsObjectivesCount[i] + ' objectives in the interaction found ' + lmsCount + '</p>');
+            } else {
+                output.append('<p>Interaction objectives count correct: ' + interactionsObjectivesCount[i] + '</p>');
+            }
+
+            lmsCount = API.LMSGetValue('cmi.interactions.'+i+'.correct_responses._count');
+            if (lmsCount !== interactionsObjectivesCount[i]) {
+                output.append('<p style="color: red">Expecting ' + interactionsObjectivesCount[i] + ' correct responses in the interaction found ' + lmsCount + '</p>');
+            } else {
+                output.append('<p>Interaction correct responses count correct: ' + interactionsObjectivesCount[i] + '</p>');
+            }
+        }
+
+    }
+
     // Set-up everything.
     $(document).ready(function(){
         initAPI();
@@ -309,6 +476,10 @@
         $("#create-objectives").on('click', function(e) {
             objectivesDialog.dialog("open");
             $("#objective-number").val(getElement("cmi.objectives._count"));
+        });
+
+        $("#launch-tests").button().on('click', function(e) {
+            launchTests()
         });
 
     });
